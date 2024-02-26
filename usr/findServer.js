ea32e722a6d3f822b551c8745931e1ba672202c2
Afloatbus <91826947+Afloatbus@users.noreply.github.com>
2024-02-26 22:33:51 +0000

    created findServer.js

--- usr/findServer.js
+++ usr/findServer.js
@@ -1,4 +1,16 @@
-/** @param {NS} ns */
+/** @param {import("..").NS} ns */
 export async function main(ns) {
 	ns.disableLog("ALL"); ns.tail(); ns.clearLog();
+
+	const path = [ns.args[0]];
+
+	// this might look like it won't work but it does
+	// this is because the first result of ns.scan is always closer to home
+	// this means you can start scanning from the target server and work your way back to home
+	while (path[0] !== "home") path.unshift(ns.scan(path[0])[0]);
+	
+	// ;connect allows clicking to connect
+	if (ns.fileExists("AutoLink.exe")) ns.tprint(path.join(";connect -> "));
+	else ns.tprint(path.join(" -> "));
 }
+export const autocomplete = (data) => data.servers;

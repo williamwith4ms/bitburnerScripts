38049f4bc8133ff0607023f3c9d49461febb20f0
Afloatbus <91826947+Afloatbus@users.noreply.github.com>
2024-02-26 22:44:30 +0000

    made

--- usr/findServer.js
+++ usr/findServer.js
@@ -1,6 +1,6 @@
 /** @param {import("..").NS} ns */
 export async function main(ns) {
-	ns.disableLog("ALL"); ns.tail(); ns.clearLog();
+	ns.disableLog("ALL");
 
 	const path = [ns.args[0]];
 
@@ -8,6 +8,7 @@ export async function main(ns) {
 	// this is because the first result of ns.scan is always closer to home
 	// this means you can start scanning from the target server and work your way back to home
 	while (path[0] !== "home") path.unshift(ns.scan(path[0])[0]);
-	ns.tprint(path.join(" -> "));
+	path.shift();
+	ns.tprint(`;connect ${path.join(";connect ")}`);
 }
 export const autocomplete = (data) => data.servers;

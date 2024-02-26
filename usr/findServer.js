ffa77ee1a254dded9f3ce5a0b292ab5f1a271539
Afloatbus <91826947+Afloatbus@users.noreply.github.com>
2024-02-26 22:35:25 +0000

    Remove unnecessary code and update tprint message

--- usr/findServer.js
+++ usr/findServer.js
@@ -8,9 +8,6 @@ export async function main(ns) {
 	// this is because the first result of ns.scan is always closer to home
 	// this means you can start scanning from the target server and work your way back to home
 	while (path[0] !== "home") path.unshift(ns.scan(path[0])[0]);
-	
-	// ;connect allows clicking to connect
-	if (ns.fileExists("AutoLink.exe")) ns.tprint(path.join(";connect -> "));
-	else ns.tprint(path.join(" -> "));
+	ns.tprint(path.join(" -> "));
 }
 export const autocomplete = (data) => data.servers;
